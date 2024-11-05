0c44d7ff539b64a6c0932a8282a51dee3515b3cd
unknown <Kopiszka31@gmail.com>
2024-11-05 11:32:54 +0100

    -5.11

---
+++ Egzaminacyjne/Sieci komputerowe/skrypt.js
@@ -0,0 +1,11 @@
+const aktu = document.querySelectorALL('.aktu')
+const zamo = document.querySelectorALL('.zamo')
+const pro = document.querySelectorALL('.pro')
+const ilosc = document.querySelectorALL('.ilosc')
+
+
+function CheckQ(){
+    
+}
+
+CheckQ();
