7a37179d7b826f5e252d4715d010a6c4a8353bb4
Jakub <kopiszka31@gmail.com>
2025-01-13 21:56:41 +0100

    13.01

---
+++ czas.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Czas</h1>
+    <?php
+    $data=date("m-d");
+    $data1=date("m-d-Y");
+    $data2=date("l");
+
+    echo "$data"; echo "<br>";  
+    echo "$data1";     echo "<br>";
+    echo "$data2";
+
+    ?>
+</body>
+</html>
